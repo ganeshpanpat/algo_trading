a2758c6a6514b6d60f718465e01d0a9311b9fc1e
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2024-09-20 11:22:32 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -576,11 +576,11 @@ def get_trade_info(df):
         if int(df.iloc[i]['RSI']) >= 60 and int(df.iloc[i-1]['RSI']) < 60:df.loc[i, 'RSI_60 Trade'] = "Buy"
         if int(df.iloc[i]['RSI_9']) >=  int(df.iloc[i]['WMA_RSI_9']) and int(df.iloc[i-1]['RSI_9']) <=  int(df.iloc[i-1]['WMA_RSI_9']) :df.loc[i, 'RSI_WMA_9 Trade'] = "Buy"
 
-        start_index = -5;end_index = -1
+        start_index = -5;end_index = -2
         subset_df =df.iloc[start_index:end_index]
         max_high = subset_df['Close'].max()
         min_low = subset_df['Close'].min()
-        if df.iloc[-1]['Close'] >= max_high:df.loc[-1, 'High Break Trade'] = "Buy"
+        if df.iloc[i]['Close'] >= max_high:df.loc[-1, 'High Break Trade'] = "Buy"
 
         for indicator_trade in indicator_list:
             if df[indicator_trade][i] == "Buy":
