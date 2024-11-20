a168227a4f3c87c892525b077f34caff9ebbed7a
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2024-11-20 15:34:47 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -868,9 +868,9 @@ def sub_loop_code(now_minute):
       if 'OPT:5M' in time_frame_interval:
         trade_near_options('5m')
         log_holder.dataframe(st.session_state['options_trade_list'],hide_index=True)
-      if 'STK:5M' in time_frame_interval:
+      if 'STK:5M' in time_frame_interval
         for symbol in st.session_state['fut_list']:
-          index_trade(symbol+"-EQ","5m")
+          index_trade(symbol+".NS","5m")
         log_holder.dataframe(st.session_state['options_trade_list'],hide_index=True)
     if (now_minute%15==0 and 'IDX:15M' in time_frame_interval):
       for symbol in index_list:index_trade(symbol,"15m")
