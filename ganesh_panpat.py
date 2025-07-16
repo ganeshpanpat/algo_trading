f74c5acbb405c65ce5efd1a00cc6250c0c8c7f80
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2025-07-16 15:10:47 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -791,6 +791,7 @@ def check_target_sl():
         df=get_historical_data(symbol=tradingsymbol,interval="5m",token=symboltoken,exch_seg=exchange)
         trade=str(df['Trade'].values[-1])
         buy_df['ordertag'].iloc[i]=df['Supertrend_10_1'].values[-1]
+        if trade=="Sell":buy_df['ordertag'].iloc[i]=df['Trade'].values[-1]
         #if trade=="Sell":exit_position(symboltoken,tradingsymbol,exchange,qty,ltp_price,ordertag='')
       except:pass
   todays_trade_df.dataframe(buy_df[['updatetime','tradingsymbol','price','Stop Loss','Target','LTP','Status','Sell','Exit Time','Profit','Profit %','ordertag','Sell Indicator']],hide_index=True)
