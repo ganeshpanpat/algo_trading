0b5172735bc01218ddb27c52049b178afe6ab0a7
ganeshpanpat <99556947+ganeshpanpat@users.noreply.github.com>
2025-07-25 15:04:54 +0530

    Update ganesh_panpat.py

--- ganesh_panpat.py
+++ ganesh_panpat.py
@@ -788,7 +788,7 @@ def get_todays_trade(orderbook):
     st.session_state['todays_trade']=buy_df
     st.session_state['todays_trade_pnl']=int(sum(buy_df['Profit']))
   except: pass
-  check_target_sl()
+
 def check_target_sl():
   buy_df=st.session_state['todays_trade']
   for i in range(0,len(buy_df)):
@@ -802,11 +802,11 @@ def check_target_sl():
         df=get_historical_data(symbol=tradingsymbol,interval="5m",token=symboltoken,exch_seg=exchange)
         trade=str(df['Trade'].values[-1])
         buy_df['Stop Loss'].iloc[i]=int(df['Supertrend_10_1'].values[-1])
-        buy_df['Target'].iloc[i]=df['Close'].values[-1]
+        buy_df['Target'].iloc[i]=df['Close'].values[-1]-df['Supertrend_10_1'].values[-1]
         if trade=="Sell" or int(df['Supertrend_10_1'].values[-1])>int(df['Close'].values[-1]) or profit > 1000:
           buy_df['ordertag'].iloc[i]="Sell:" + str(df['Supertrend_10_1'].values[-1])
           ltp_price=get_ltp_price(symbol=tradingsymbol,token=symboltoken,exch_seg=exchange)
-          orderId=exit_position(symboltoken,tradingsymbol,exchange,qty,ltp_price,ordertag='')
+          #orderId=exit_position(symboltoken,tradingsymbol,exchange,qty,ltp_price,ordertag='')
           buy_df['Sell Indicator'].iloc[i]=orderId
           buy_df['Status'].iloc[i]="Closed"
       except:pass
